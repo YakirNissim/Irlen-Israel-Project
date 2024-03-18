f812a122c3e6f461eec07682b6acc597c9922eb4
Yakir Nissim <101890349+YakirNissim@users.noreply.github.com>
2024-03-18 14:00:21 +0200

    Delete Modules directory

--- Modules/OrdersManagerPackage/__init__.py
+++
@@ -1,2 +0,0 @@
-# from Modules.OrdersManagerPackage.OrdersManager import OrdersManager
-from Modules.OrdersManager import OrdersManager
--- Modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from Modules.ExceptionsAndGlobalFunctions import *
-from Modules.OrdersManager import OrdersManager
-from Modules.CustomerManager import CustomerManager
